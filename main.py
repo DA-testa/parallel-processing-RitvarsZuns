93c8db487e0c49a4734ff789dc9818e89120a592
RitvarsZuns <ritvars.zuns@edu.rtu.lv>
2023-03-10 20:13:39 +0200

    uzd 4

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # python3
+import heapq
 
 def parallel_processing(n, m, data):
     output = []
+    threads = [(0, i) for i in range(n)] # (completion_time, index)
+    heapq.heapify(threads)
+
+    for job_time in data:
+        completion_time, thread_idx = heapq.heappop(threads)
+        output.append((thread_idx, completion_time))
+        heapq.heappush(threads, (completion_time + job_time, thread_idx))
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
 
@@ -13,15 +21,17 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
+
+    for thread_idx, start_time in result:
+        print(thread_idx, start_time)
     
     # TODO: print out the results, each pair in it's own line
 
